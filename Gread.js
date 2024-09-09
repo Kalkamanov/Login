ec4b2f1f8377b78366aa844bab0b5b62b6b253da
Nurik <n_kalkamanov@mail.ru>
2024-09-09 14:07:46 +0500

    Login

---
+++ Gread.js
@@ -0,0 +1,19 @@
+// let user = {
+//     name: 'Nurik',
+//     age: 24,
+//     getLocation() {
+//         return {
+//             country: 'Kazakhstan',
+//             city: 'Taraz'
+            
+//         } 
+//     }
+// }
+
+// console.log(user.)
+
+
+
+
+
+
